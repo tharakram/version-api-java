bf179ef4f57be175fb3026aa4eb09d9a09fa8984
Tharakarama Reddy <tharakram.ece@gmail.com>
2022-02-01 22:54:03 +0530

    Initial commit of VersionAPI

---
+++ src/main/java/com/app/version/VersionData.java
@@ -0,0 +1,18 @@
+package com.app.version;
+
+public class VersionData {
+
+    private String version;
+
+    public VersionData(String version) {
+        this.version = version;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
---
+++ src/test/java/com/app/version/VersionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.version;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
